f7587cf712b3404896e04041d3bd8d3579a5630b
Peccator13 <kadiroz-10-2006@protonmail.com>
2026-05-22 23:02:42 +0300

    Add files via upload

---
+++ türkay.py
@@ -0,0 +1,69 @@
+print("selamün aleykümm")
+durum = input("nasılsın?:")
+if durum == "iyiyim":
+    print("ne güzeell, hep iyi ol:)")
+elif durum == "iyi":
+    print("ne güzeell, hep iyi ol:)")
+elif durum == "iyii":
+    print("ne güzeell, hep iyi ol:)")
+elif durum == "iyiii":
+    print("ne güzeell, hep iyi ol:)")
+elif durum == "iyiyim sen nasılsın":
+    print("teşekkürlerr, seni gördüm daha iyi oldum xD")
+elif durum == "iyidir sen nasılsın":
+    print("teşekkürlerr, seni gördüm daha iyi oldum xD")
+elif durum == "iyi sen nasılsın":
+    print("teşekkürlerr, seni gördüm daha iyi oldum xD")
+elif durum == "iyii sen nasılsın":
+    print("teşekkürlerr, seni gördüm daha iyi oldum xD")
+elif durum == "iyiii sen nasılsın":
+    print("teşekkürlerr, seni gördüm daha iyi oldum xD")
+elif durum == "aynı":
+    print("ooo yorucu değil mi bu kadar iyi gözükmek")
+elif durum == "aynı işte":
+    print("ooo yorucu değil mi bu kadar iyi gözükmek")
+elif durum == "normal":
+    print("unutma normal yokturr ;)")
+elif durum == "idare eder":
+    print("umarım en güzel şekilde idare ederrr")
+elif durum == "şükür iyi ya":
+    print("şükür şükür allah iyilik versinn =I")
+elif durum == "elhamdülillah":
+    print("hhmmm yani şimdi iyi misin, değil misin -_-")
+elif durum == "iyiyim ya":
+    print("ne güzeell, hep iyi ol:)")
+elif durum == "iyi ya":
+    print("ne güzeell, hep iyi ol:)")
+elif durum == "iyiyim şükür":
+    print("şükür şükür allah iyilik versinn")
+elif durum == "kötü":
+    print("öhm öhm bence sorunu kesinlikle Kadire anlatmalısın")
+elif durum == "kötüyüm":
+    print("öhm öhm bence sorunu kesinlikle Kadire anlatmalısın")
+elif durum == "kötü gibiyim":
+    print("öhm öhm bence sorunu kesinlikle Kadire anlatmalısın")
+elif durum == "kötü be":
+    print("öhm öhm bence sorunu kesinlikle Kadire anlatmalısın")
+elif durum == "bok gibi":
+    print("bence bir bok bu kadar güzel olamaz ;)")
+elif durum == "pekte iyi değil":
+    print("sabret, sabret elbet güzel olur")
+elif durum == "sence ay nasıl":
+    print("sen soruyorsan çok güzel, peki ya sence?")
+elif durum == "bok gibiyim":
+    print("öhm öhm bence sorunu kesinlikle Kadire anlatmalısın")
+elif durum == "eh işte":
+    print("ne demek eh işte -_-, git Kadire dökül bakalım hade.")
+elif durum == "açık açık söyler misin":
+    print("Seni Çok ama Çok Seviyorum")
+elif durum == "şiir kitabı kime yazıldı?":
+    print("sana yazıldı")
+elif durum == "gayet iyi":
+    print("daha da iyi olursun inşallaahhh")
+elif durum == "gayet iyii":
+    print("daha da iyi olursun inşallaahhh")
+elif durum == "gayet iyiii":
+    print("daha da iyi olursun inşallaahhh")
+else:
+    print("anlamıyorumm 30 ihtimal yazdım ve nasıl hiçbiri tutmaz yaww!!!")
+input("\nProgramdan çıkmak istersen enter'a basman yeterlii :I")
